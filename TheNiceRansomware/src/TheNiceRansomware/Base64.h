93d85c68ce030f9311060f74db233a2e8a400d16
Dor <39267196+dor-alt@users.noreply.github.com>
2019-01-02 03:10:47 +0200

    added isBase64

--- TheNiceRansomware/src/TheNiceRansomware/Base64.h
+++ TheNiceRansomware/src/TheNiceRansomware/Base64.h
@@ -10,4 +10,5 @@ public:
 	static std::string encode(const std::vector<BYTE>& buf);
 	static std::string encode(const BYTE* buf, unsigned int bufLen);
 	static std::vector<BYTE> decode(std::string encoded_string);
+	static bool isBase64(string s);
 };
